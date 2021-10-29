9a7a3fb651e42955149be12583977107aca9b681
Jardel Urban <jardelurban3@gmail.com>
2021-10-29 14:57:46 -0300

    Alterando .map para <FlatList />

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ import {
   Text, 
   StyleSheet, 
   TextInput, 
-  Platform
+  Platform,
+  FlatList
 } from 'react-native';
 
 import { Button } from '../components/Button';
@@ -37,11 +38,13 @@ export function Home(){
         My skills
       </Text>
 
-      {
-        mySkills.map(skill => (
-          <SkillCard skill={skill}/>
-        ))
-      }
+      <FlatList 
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <SkillCard skill={item}/>
+        )}
+      />
     </View>
   )
 }
