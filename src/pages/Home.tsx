164f3b4c904e1093293a0429ca0359c7fce9d4fc
Jardel Urban <jardelurban3@gmail.com>
2021-10-29 17:04:31 -0300

    Adicionando função de remoção de skill

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,6 +29,12 @@ export function Home(){
     setMySkills(old => [...old, data])
   } 
 
+  function handleRemoveSkill(id: string){
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ))
+  }
+
   useEffect (() => {
     const currentHour = new Date().getHours();
     
@@ -74,7 +80,10 @@ export function Home(){
         showsVerticalScrollIndicator={false}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item.name}/>
+          <SkillCard 
+            skill={item.name}
+            onPress = {() => handleRemoveSkill(item.id)}
+          />
         )}
       />
     </View>
