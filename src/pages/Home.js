6486febf03e31911d71ff695e620150e102cad44
Jardel Urban <jardelurban3@gmail.com>
2021-10-29 16:11:42 -0300

    Adicionando greetings com useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   View, 
   Text, 
@@ -14,15 +14,34 @@ import { SkillCard } from '../components/SkillCard';
 export function Home(){
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
 
   function handleAddNewSkill() {
     setMySkills(old => [...old, newSkill])
   } 
 
+  useEffect (() => {
+    const currentHour = new Date().getHours();
+    
+    if (currentHour < 12){
+      setGreeting("Good morning")
+    } else if (currentHour >= 12 && currentHour < 18){
+      setGreeting("Good afternoon")
+    } else {
+      setGreeting("Good night")
+    } 
+
+  }, []) 
+
   return (
     <View style={styles.container}>
+      
       <Text style={styles.title}>
         Welcome, Jardel
+      </Text>
+
+      <Text style={styles.greetings}>
+        {greeting}
       </Text>
 
       <TextInput 
@@ -40,6 +59,7 @@ export function Home(){
 
       <FlatList 
         data={mySkills}
+        showsVerticalScrollIndicator={false}
         keyExtractor={item => item}
         renderItem={({ item }) => (
           <SkillCard skill={item}/>
@@ -68,5 +88,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7
+  },
+  greetings: {
+    color: '#fff'
   }
 })
